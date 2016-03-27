5077b816ec580f131000d47cf35857fb41647e74
Наташа Неровня <natanerovnja@gmail.com>
2016-03-27 17:04:02 +0300

    Add console tests

---
+++ PolynomialTests/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polynomial;
+//using Polynomial = Polynomial.Polynomial;
+
+
+namespace PolynomialTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynomial.Polynomial pol = new Polynomial.Polynomial(3, 0, 2);
+            Polynomial.Polynomial pol2 = new Polynomial.Polynomial(0, 1, 5, 6);
+            Console.WriteLine("pol1 : " + pol.ToString());
+            Console.WriteLine("pol2: " + pol2.ToString());
+            Polynomial.Polynomial pol3 = pol2 - pol;
+            Console.WriteLine("pol1 - pol2 : " + pol3.ToString());
+            Polynomial.Polynomial pol4 = pol2 * pol;
+            Console.WriteLine("pol1 * pol2 : " + pol4.ToString());
+            Polynomial.Polynomial pol5 = pol2 / pol;
+            Console.WriteLine("pol1 / pol2 : " + pol5.ToString());
+            Console.ReadKey();
+        }
+    }
+}
